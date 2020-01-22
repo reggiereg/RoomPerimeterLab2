d143c4af904687a308236c061a537e7e16d9aaca
Reginald Richardson <youngreg21@gmail.com>
2020-01-22 10:00:53 -0500

    Lab2 push for room perimeter lab

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char option = 'y';
+            while (option != 'n')
+            {
+                decimal length, width, perimeter, area, height;
+                Console.WriteLine("Welcome to the Grand Circus Room Detail Generator");
+                Console.WriteLine("Please enter the Length: ");
+                length = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the Width: ");
+                width = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the Height: ");
+                height = decimal.Parse(Console.ReadLine());
+                Console.WriteLine($"Area: {length * width}\nPerimeter: {length * 2 + width *2}\nVolume: {length*width*height}");
+                Console.WriteLine("\n");
+                Console.WriteLine("Would you like to continue? (y/n)");
+                option = char.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
